bbced615a298db5ed61f7656d004da502cb75e70
Alejandro Sanchez <aalejo@gmail.com>
2020-04-20 17:57:01 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+stop = False
+    while stop == False:
+        print("""
+    Choose an option: 
+        1. Add one task
+        2. Delete a task
+        3. Print the current list of tasks
+        4. Save todo's to todos.csv
+        5. Load todo's from todos.csv
+        6. Exit
+    """)
